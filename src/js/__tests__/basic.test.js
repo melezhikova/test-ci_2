a5d1319faa488671f444402dc4e0bcf331b88e69
Liudmila Melezhikova <melezhikova@yandex.ru>
2021-08-22 11:42:52 +0500

    second commit

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,6 +1,23 @@
 import itemsSort from '../app';
 
-test('should compare items in array', () => {
+test('should compare items in array_toEqual', () => {
+  const input = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник2', health: 10 }
+  ];
+  const result = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+    { name: 'мечник2', health: 10 }
+  ];
+  const expected = itemsSort(input);
+  expect(result).toEqual(expected);
+});
+
+test('should compare items in array_toBe', () => {
   const input = [
     { name: 'мечник', health: 10 },
     { name: 'маг', health: 100 },
@@ -12,5 +29,5 @@ test('should compare items in array', () => {
     { name: 'мечник', health: 10 },
   ];
   const expected = itemsSort(input);
-  expect(result).toEqual(expected);
+  expect(result[1].health).toBe(expected[1].health);
 });
